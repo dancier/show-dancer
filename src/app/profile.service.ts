45158a1b12b8e34b2015a5b1fd6f43927b5e201c
Dominik Halfkann <dominik.halfkann@mailbox.org>
2021-04-21 18:58:14 +0200

    #2 add configurable base url for the dancier backend

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -2,20 +2,21 @@ import { Injectable } from '@angular/core';
 import { Profile } from './profile/profile';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  options = {withCredentials:true}
+  options = {withCredentials:true};
 
-  private dancerUrl = "https://dancer.dancier.net/profile"
+  private dancerUrl = `${environment.dancerUrl}/profile`;
 
   constructor(private http: HttpClient) { }
 
   get(): Observable<Profile> {
 
-    return this.http.get<Profile>(this.dancerUrl, this.options)
+    return this.http.get<Profile>(this.dancerUrl, this.options);
   }
 }
