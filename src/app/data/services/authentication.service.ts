88c31465cf247f0be5b95d58a8fdd39485a0b5ca
Dominik Halfkann <dominik.halfkann@mailbox.org>
2022-05-27 20:04:57 +0200

    remove error log

--- src/app/data/services/authentication.service.ts
+++ src/app/data/services/authentication.service.ts
@@ -51,7 +51,6 @@ export class AuthenticationService {
       .pipe(
         map((_): LoginResponse => 'SUCCESS'),
         catchError((error: HttpErrorResponse): Observable<LoginResponse> => {
-          console.log('error', error);
           switch(error.status) {
             case 401:
               return of('INCORRECT_CREDENTIALS');
