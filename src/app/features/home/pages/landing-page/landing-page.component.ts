937dfa0cdebcc62c65a15d2be551d15284c0d861
Xiaofei Gorzala <xiaofei.gorzala@zalando.de>
2022-05-31 14:11:37 +0200

    Improved the layouts

--- src/app/features/home/pages/landing-page/landing-page.component.ts
+++ src/app/features/home/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class LandingPageComponent {
 
-  constructor() { }
+  constructor() {
+  }
 
 }
