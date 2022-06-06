362b14650144e8d6079422feb90d726c1a1859c2
Xiaofei Gorzala <xiaofei.gorzala@zalando.de>
2022-06-06 14:55:15 +0200

    Show hint text on personal edit form dynamically.

--- src/app/features/home/pages/edit-personal-data/edit-personal-data.component.ts
+++ src/app/features/home/pages/edit-personal-data/edit-personal-data.component.ts
@@ -7,7 +7,44 @@ import { Component } from '@angular/core';
 })
 export class EditPersonalDataComponent {
 
+  isBirthdayFieldOnFocus: boolean = false;
+  isGenderFieldOnFocus: boolean = false;
+  isHeightFieldOnFocus: boolean = false;
+  isZipFieldOnFocus: boolean = false;
+
   constructor() {
+  }
+
+  birthdayFieldOnFocus() {
+    this.isBirthdayFieldOnFocus = true;
+  }
+
+  birthdayFieldOnBlur() {
+    this.isBirthdayFieldOnFocus = false;
+  }
+
+  genderFieldOnFocus() {
+    this.isGenderFieldOnFocus = true;
+  }
+
+  genderFieldOnBlur() {
+    this.isGenderFieldOnFocus = false;
+  }
+
+  heightFiledOnFocus() {
+    this.isHeightFieldOnFocus = true;
+  }
+
+  heightFieldOnBlur() {
+    this.isHeightFieldOnFocus = false;
+  }
+
+  zipFieldOnFocus() {
+    this.isZipFieldOnFocus = true;
+  }
+
+  zipFieldOnBlur() {
+    this.isZipFieldOnFocus = false;
   }
 
 }
