8b1144a8518c2babf45184f72847c0e28774167b
Dominik Halfkann <dominik.halfkann@mailbox.org>
2022-07-02 15:07:19 +0200

    #57 fix eslint

--- src/app/features/registration/components/resend-verification-link-form/resend-verification-link-form.component.ts
+++ src/app/features/registration/components/resend-verification-link-form/resend-verification-link-form.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resend-verification-link-form',
   templateUrl: './resend-verification-link-form.component.html',
   styleUrls: ['./resend-verification-link-form.component.scss']
 })
-export class ResendVerificationLinkFormComponent implements OnInit {
+export class ResendVerificationLinkFormComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
