5a2909fd528f91e8a9b9524b036b86a871ec108e
Xiaofei Gorzala <xiaofei.gorzala@zalando.de>
2022-07-20 14:43:34 +0200

    Fixed lint

--- src/app/features/home/components/beta-signup-banner/beta-signup-banner.component.ts
+++ src/app/features/home/components/beta-signup-banner/beta-signup-banner.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-beta-signup-banner',
   templateUrl: './beta-signup-banner.component.html',
   styleUrls: ['./beta-signup-banner.component.scss']
 })
-export class BetaSignupBannerComponent implements OnInit {
+export class BetaSignupBannerComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
+
 
 }
