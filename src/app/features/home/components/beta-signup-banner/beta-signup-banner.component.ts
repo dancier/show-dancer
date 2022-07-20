250b233cf49143e0016a08221ecf28ca181022b6
Xiaofei Gorzala <xiaofei.gorzala@zalando.de>
2022-07-20 13:46:41 +0200

    Added banner; fixed some small issues; added icons

---
+++ src/app/features/home/components/beta-signup-banner/beta-signup-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beta-signup-banner',
+  templateUrl: './beta-signup-banner.component.html',
+  styleUrls: ['./beta-signup-banner.component.scss']
+})
+export class BetaSignupBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
