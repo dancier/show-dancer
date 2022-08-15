3ac199266459ea51f66c8f1e37d903d68a0ff4b1
Dominik Halfkann <dominik.halfkann@mailbox.org>
2022-08-15 18:09:21 +0200

    #113 migrate from Karma to Jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { paths } = require('./tsconfig.json').compilerOptions;
+
+// eslint-disable-next-line no-undef
+globalThis.ngJest = {
+  skipNgcc: false,
+  tsconfig: 'tsconfig.spec.json',
+};
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  transform: { '^.+.(ts|mjs|js|html)$': 'jest-preset-angular' },
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
+};
