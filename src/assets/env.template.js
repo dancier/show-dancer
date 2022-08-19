3d9f3a37d8911ac9c219baaa9bc7d582d2ce5f24
Dominik Halfkann <dominik.halfkann@mailbox.org>
2022-08-19 20:20:46 +0200

    #113 remove unnecessary code

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -1,7 +1,5 @@
 // set environment variables in global window object
 (function(window) {
-  window.env = window.env || {};
-
   window.env = {
     // environment variables
     apiUrl: '${DANCIER_BACKEND_URL}'
