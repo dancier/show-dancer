5c3cbb580152eb6cf435bcf606c5263f2dc5d790
Jan Stroppel <jan.stroppel@web.de>
2022-08-20 16:05:18 +0200

    fixes after master merge

--- src/app/data/services/profile-http.service.ts
+++ src/app/data/services/profile-http.service.ts
@@ -1,7 +1,4 @@
-import {
-  HttpClient,
-  HttpErrorResponse,
-} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NameAvailability, Profile } from '@data/types/profile.types';
 import { APIResponse } from '@data/types/shared.types';
@@ -19,9 +16,10 @@ export class ProfileHttpService {
 
   constructor(
     private http: HttpClient,
-    private environment: EnvironmentService) {
-      this.baseUrl = `${this.environment.getApiUrl()}/profile`;
-    }
+    private environment: EnvironmentService
+  ) {
+    this.baseUrl = `${this.environment.getApiUrl()}/profile`;
+  }
 
   getProfile$(): Observable<APIResponse | Profile> {
     return this.http.get<Profile>(`${this.baseUrl}`, this.defaultOptions).pipe(
@@ -35,25 +33,35 @@ export class ProfileHttpService {
   }
 
   updateProfile$(profile: Profile): Observable<APIResponse | Profile> {
-    return this.http.put<Profile>(`${this.baseUrl}`, profile, this.defaultOptions).pipe(
-      catchError((error: HttpErrorResponse): Observable<APIResponse> => {
-        switch (error.status) {
-          default:
-            return of('SERVER_ERROR');
-        }
-      })
-    );
+    return this.http
+      .put<Profile>(`${this.baseUrl}`, profile, this.defaultOptions)
+      .pipe(
+        catchError((error: HttpErrorResponse): Observable<APIResponse> => {
+          switch (error.status) {
+            default:
+              return of('SERVER_ERROR');
+          }
+        })
+      );
   }
 
   checkNameAvailability$(dancerName: string): Observable<APIResponse> {
-    return this.http.get<NameAvailability>(`${this.baseUrl}/checkDancerNameAvailibility/${dancerName}`, this.defaultOptions).pipe(
-      map((availability): APIResponse => availability ? 'SUCCESS' : 'NOT_AVAILABLE'),
-      catchError((error: HttpErrorResponse): Observable<APIResponse> => {
-        switch (error.status) {
-          default:
-            return of('SERVER_ERROR');
-        }
-      })
-    );
+    return this.http
+      .get<NameAvailability>(
+        `${this.baseUrl}/checkDancerNameAvailibility/${dancerName}`,
+        this.defaultOptions
+      )
+      .pipe(
+        map(
+          (availability): APIResponse =>
+            availability ? 'SUCCESS' : 'NOT_AVAILABLE'
+        ),
+        catchError((error: HttpErrorResponse): Observable<APIResponse> => {
+          switch (error.status) {
+            default:
+              return of('SERVER_ERROR');
+          }
+        })
+      );
   }
 }
