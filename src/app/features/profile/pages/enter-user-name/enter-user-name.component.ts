f3b706cde336aea369e392975d505e0900da1808
Dominik Halfkann <dominik.halfkann@mailbox.org>
2022-09-04 10:34:03 +0200

    use typed form for "set username" in wizard

--- src/app/features/profile/pages/enter-user-name/enter-user-name.component.ts
+++ src/app/features/profile/pages/enter-user-name/enter-user-name.component.ts
@@ -1,47 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  UntypedFormBuilder,
-  UntypedFormGroup,
-  Validators,
-} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProfileHttpService } from '@data/services/profile-http.service';
 import { ProfileService } from '@data/services/profile.service';
 import { APIError } from '@data/types/response.types';
-import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-enter-user-name',
   templateUrl: './enter-user-name.component.html',
   styleUrls: ['./enter-user-name.component.scss'],
 })
-export class EnterUserNameComponent implements OnInit {
-  usernameForm!: UntypedFormGroup;
+export class EnterUserNameComponent {
+  usernameForm = this.fb.group({
+    username: ['', [Validators.required]],
+  });
   error?: APIError;
-  
+
   constructor(
     public profileDataService: ProfileService,
     private profileHttpService: ProfileHttpService,
-    private fb: UntypedFormBuilder,
+    private fb: FormBuilder,
     private router: Router
   ) {}
 
-  ngOnInit(): void {
-    this.initReactiveForm();
-  }
-
-  private initReactiveForm(): void {
-    this.usernameForm = this.fb.group({
-      username: ['', [Validators.required]],
-    });
-  }
-
-  public errorHandling(control: string, error: string): boolean {
-    return this.usernameForm.controls[control].hasError(error);
+  public hasUsernameFieldError(error: string): boolean {
+    return this.usernameForm.controls.username.hasError(error);
   }
 
   submitForm(): void {
-    if (this.usernameForm.valid) {
+    if (this.usernameForm.valid && this.usernameForm.value.username) {
       const username = this.usernameForm.value.username;
       this.profileHttpService
         .checkNameAvailability$(username)
