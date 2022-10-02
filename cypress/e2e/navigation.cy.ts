b5efd832fc8f9bbde6ea3e79850237efcc16ccef
Dominik Halfkann <dominik.halfkann@mailbox.org>
2022-10-02 13:14:15 +0200

    modify acceptance test for about-us

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,6 +1,6 @@
 describe('Pages that are publicly visible without login should be accessible through navigation', () => {
   it('passes', () => {
-    cy.visit('https://test.dancier.net');
+    cy.visit('');
   })
 
   it('can navigate to the "about us" page', () => {
