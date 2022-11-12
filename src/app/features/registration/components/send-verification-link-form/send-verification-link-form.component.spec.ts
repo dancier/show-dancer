6b15c6d85b22e7f77a0974eedfda45489d2ce4e2
Jan Stroppel <jan.stroppel@web.de>
2022-11-12 18:27:14 +0100

    fixed test

--- src/app/features/registration/components/send-verification-link-form/send-verification-link-form.component.spec.ts
+++ src/app/features/registration/components/send-verification-link-form/send-verification-link-form.component.spec.ts
@@ -4,12 +4,26 @@ import { SendVerificationLinkFormComponent } from '@features/registration/compon
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MockProvider } from 'ng-mocks';
+import { AuthenticationService } from '@core/auth/services/authentication.service';
+import { ActivatedRoute } from '@angular/router';
 
 describe('SendVerificationLinkFormComponent', () => {
   let spectator: Spectator<SendVerificationLinkFormComponent>;
   const createComponent = createComponentFactory({
     component: SendVerificationLinkFormComponent,
-    imports: [MatCardModule, MatFormFieldModule, MatInputModule],
+    imports: [
+      MatCardModule,
+      MatFormFieldModule,
+      MatInputModule,
+      ReactiveFormsModule,
+    ],
+    providers: [
+      MockProvider(AuthenticationService),
+      MockProvider(AuthenticationService),
+      MockProvider(ActivatedRoute),
+    ],
   });
 
   beforeEach(() => (spectator = createComponent()));
