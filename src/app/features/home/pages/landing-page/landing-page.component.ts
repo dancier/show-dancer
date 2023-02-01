a88298099178d31f1dff4d904bfc713384357a9c
Dominik Halfkann <dominik.halfkann@mailbox.org>
2023-02-01 20:39:50 +0100

    add hero section in header

--- src/app/features/home/pages/landing-page/landing-page.component.ts
+++ src/app/features/home/pages/landing-page/landing-page.component.ts
@@ -1,35 +1,15 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
 })
-export class LandingPageComponent implements OnInit {
-  windowHeight: number = 0;
-  landingPageContentMinHeight: number = 0;
-  navigationHeight = 64;
-  footerHeight = 218;
-  textContentHeight = 64;
+export class LandingPageComponent {
+  constructor(private router: Router) {}
 
-  constructor() {}
-
-  ngOnInit(): void {
-    this.windowHeight = window.innerHeight;
-    this.calculateLandingPageContentMinHeight();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(): void {
-    this.windowHeight = window.innerHeight;
-    this.calculateLandingPageContentMinHeight();
-  }
-
-  private calculateLandingPageContentMinHeight(): void {
-    this.landingPageContentMinHeight =
-      this.windowHeight -
-      this.navigationHeight -
-      this.footerHeight -
-      this.textContentHeight;
+  navigateToRegistration(): void {
+    this.router.navigate(['/registration']);
   }
 }
