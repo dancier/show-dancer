be45f346c6f03cd44ff84656d2e31de62531f4b5
Dominik Halfkann <dominik.halfkann@mailbox.org>
2023-02-04 15:34:33 +0100

    refactor for accessibility, add event for navigation, remove unnecessary prettier settings (we don't want to override the defaults with exactly the same values)

--- src/app/features/home/pages/landing-page/landing-page.component.ts
+++ src/app/features/home/pages/landing-page/landing-page.component.ts
@@ -1,15 +1,19 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { EventLogService } from '@core/logging/event-log.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private eventService: EventLogService) {}
 
   navigateToRegistration(): void {
+    this.eventService.createAndPublishEvent('navigated_to_page', {
+      origin: 'landing-page-hero-cta',
+      page: 'registration',
+    });
     this.router.navigate(['/registration']);
   }
 }
