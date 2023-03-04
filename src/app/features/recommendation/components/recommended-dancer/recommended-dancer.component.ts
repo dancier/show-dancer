229d4e2e09dcb9f402e85ba19cb1e8e391eef376
Dominik Halfkann <dominik.halfkann@mailbox.org>
2023-03-04 15:44:52 +0100

    handle missing profile img

--- src/app/features/recommendation/components/recommended-dancer/recommended-dancer.component.ts
+++ src/app/features/recommendation/components/recommended-dancer/recommended-dancer.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { RecommendedDancer } from '../../types/recommended-dancers.types';
 import { ImageService } from '@core/image/image.service';
+import { EventLogService } from '@core/logging/event-log.service';
+import { AuthenticationService } from '@core/auth/services/authentication.service';
+import { AuthStorageService } from '@core/auth/services/auth-storage.service';
 
 @Component({
   selector: 'app-recommended-dancer',
@@ -11,5 +14,27 @@ export class RecommendedDancerComponent {
   @Input()
   dancer!: RecommendedDancer;
 
-  constructor(public imageService: ImageService) {}
+  imgSrc: string | undefined;
+
+  constructor(
+    public imageService: ImageService,
+    private readonly eventLogService: EventLogService
+  ) {}
+
+  ngOnInit(): void {
+    this.imgSrc = this.imageService.getDancerImageSrcOrDefault(
+      this.dancer.imageHash,
+      456
+    );
+  }
+
+  handleMissingImage($event: ErrorEvent): void {
+    this.eventLogService.createAndPublishEvent('frontend_error', {
+      context: 'RecommendedDancerComponent',
+      message: 'unable to load profile image for recommended dancer',
+      recommendedDancerId: this.dancer.id,
+      errorEventMessage: $event.message,
+    });
+    this.imgSrc = this.imageService.getDefaultDancerImage();
+  }
 }
