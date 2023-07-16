db603cc249b65de8a9019770cf865c5ec1544c6c
Dominik Halfkann <dominik.halfkann@mailbox.org>
2023-07-16 14:43:02 +0200

    remove comments

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,4 @@ module.exports = {
   moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
-  // transform: { '^.+.(ts|mjs|js|html)$': 'jest-preset-angular' },
-  // transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
 };
